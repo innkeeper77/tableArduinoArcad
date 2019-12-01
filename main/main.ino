ebd9c823b9110b8bc7cf919edd18d4eddc418834
innkeeper77 <erik.innkeeper77@gmail.com>
2019-12-01 15:46:01 -0700

    Added Nintendo controller code, test now flashes on and off when UP is pressed.

--- main/main.ino
+++ main/main.ino
@@ -2,16 +2,23 @@
 // https://learn.adafruit.com/neopixel-painter/test-neopixel-strip
 // https://github.com/allenhuffman/LEDSign
 
-// Dependencies: Adafruit_NeoPixel.h, available from the arduino IDE library manager
+// Setup:
+// Nintento SNES classic controller is connected to SDA/SCL/Power
+// See below to modify code to fit your LED setup
+// TODO: Implement code to work with other LED setups
+
+// Dependencies: Adafruit_NeoPixel.h and NintendoExtensionCtrl.h, available from the arduino IDE library manager
 #include <Adafruit_NeoPixel.h> 
-#define pinStrand 6
-#define numberLED 200
-#define maxBrightness 255
+#include <NintendoExtensionCtrl.h>
+
+SNESMiniController snes;
 
 // LED strip configuration ~~ Define here for how project is wired. Only works for rectangles!
+#define pinStrand 6
+#define maxBrightness 255
 #define numberLED 200
 #define ledPerRow 10
-const int firstLedPosition = 4; //1,2,3, or 4: cartesian quadrants (EG: 1 is upper right, 4 is bottom right)
+const int firstLedPosition = 4; // 1,2,3, or 4: cartesian quadrants (EG: 1 is upper right, 4 is bottom right)
 const bool zigZag = 1; // 0: straight (false) or 1: zigzag (true)
 
 // CALCULATED DEFINES AND VARIABLE INITIALIZATIONS
@@ -34,6 +41,14 @@ void setup()
   
   Serial.begin(9600);
   Serial.println("Setup commencing");
+
+  snes.begin();
+
+  while (!snes.connect()) 
+  {
+    Serial.println("Nintendo Controller not detected! Retry in 1 second");
+    delay(1000);
+  }
   
   // Determine additional needed parameters from user selected defines
   // Determine needed directions from layout, change bools if needed
@@ -46,7 +61,7 @@ void setup()
     colDirLeft = 1;
   }
 
-  
+  // ~~~~~~~~~~~~~~ The following setup code is non critical, and is for testing purposes ~~~~~~~~~
   // Create sample display array test
   int j = 0;
   for(int i=0; i < numberLED; i++)
@@ -119,32 +134,116 @@ void setup()
     displayArrayTestGreen[j+1] = 255;
     j = j + 3;
   }
+
+  
+  // ~~~~~~~~~~~~~~ END testing in setup ~~~~~~~~~
 }
+
+// ~~~~ Global variables here. It's fine, it's an arduino! ~~~~~
+bool buttonPressInProcess = 0;
+bool displayOn = 0;
+// Controller button state bools
+boolean padUp = 0;
+boolean padDown = 0;
+boolean padLeft = 0;
+boolean padRight = 0;
+boolean aButton = 0;
+boolean bButton = 0;
+boolean xButton = 0;
+boolean yButton = 0;
+boolean startButton = 0;
+boolean selectButton = 0;
+boolean lButton = 0;
+boolean rButton = 0;
 
 void loop() 
 {
+  /* ~~~~~~~~~~ Basic hackish testing, uncomment following, comment out the rest of the loop ~~~~~~~~~
   chase(lightStrand.Color(255, 0, 0)); // Red
   chase(lightStrand.Color(0, 255, 0)); // Green
   chase(lightStrand.Color(0, 0, 255)); // Blue
   outputArray(displayArrayTestRainbow, numberLED);
-  delay(1000);
+  delay(100);
   outputArray(displayArray, numberLED);
-  delay(1000);
+  delay(100);
   outputArray(displayArrayTestGreen, numberLED);
-  delay(1000);
+  delay(100);
   outputArray(displayArrayTestRainbow, numberLED);
-  delay(1000);
-  outputArray(displayArray, numberLED);
+  delay(100);
+   ~~~~~~~~~~ END: Basic hackish testing ~~~~~~~~~
+  */
+
+  //snes.printDebug();  // Print all of the values!
+  bool controllerReadSuccess = snes.update();  // Get new data from the controller
+  if (!controllerReadSuccess) // Error condition, no controller
+  {
+    Serial.println("Controller disconnected! Arduino delaying until retry");
+    delay(1000);
+  }
+  else // Normal
+  {
+    // Read the controller dpad (Up/Down/Left/Right) dpadUp() etc
+    padUp = snes.dpadUp();
+    padDown = snes.dpadDown();
+    padLeft = snes.dpadLeft();
+    padRight = snes.dpadRight();
+    // read the controller buttons (A/B/X/Y, Start/Select, L/R) snes.buttonA() etc
+    aButton = snes.buttonA();
+    bButton = snes.buttonB();
+    xButton = snes.buttonX();
+    yButton = snes.buttonY();
+    startButton = snes.buttonStart();
+    selectButton = snes.buttonSelect();
+    lButton = snes.buttonZL();
+    rButton = snes.buttonZR();
+  }
+
+  // If UP is pressed, switch display on or off. 
+  if (padUp && !buttonPressInProcess)
+  {
+    Serial.println("Pad:");
+    Serial.println(padUp);
+    Serial.println("displayOn:");
+    Serial.println(displayOn);
+    if (displayOn == 0) // Turn ON display
+    {
+      for(int i = 0; i < displayArraySize; i++)
+      {
+        displayArray[i] = displayArrayTestRainbow[i];
+      }
+      outputArray(displayArray, numberLED);
+      displayOn = 1;
+      Serial.println("Turning ON display");
+      Serial.println("displayOn:");
+      Serial.println(displayOn);
+    }
+    else // turn OFF display
+    {
+      // Set displayArray to blank array
+      for(int i = 0; i < displayArraySize; i++)
+      {
+        displayArray[i] = 0;
+      }
+      outputArray(displayArray, numberLED);
+      displayOn = 0;
+      Serial.println("Turning off display");
+    }
+    buttonPressInProcess = 1;
+  }
+  else if (!padUp && buttonPressInProcess) // Finished pressing button
+  {
+    buttonPressInProcess = 0;
+  }
 }
 
- static void chase(uint32_t c) 
+static void chase(uint32_t c) 
+{
+ for(uint16_t i=0; i<lightStrand.numPixels()+4; i++) 
  {
-  for(uint16_t i=0; i<lightStrand.numPixels()+4; i++) 
-  {
-    lightStrand.setPixelColor(i  , c); // Draw new pixel
-    lightStrand.setPixelColor(i-4, 0); // Erase pixel a few steps back
-    lightStrand.show();
-  }
+   lightStrand.setPixelColor(i  , c); // Draw new pixel
+   lightStrand.setPixelColor(i-4, 0); // Erase pixel a few steps back
+   lightStrand.show();
+ }
 }
 
 void outputArray(uint8_t displayArray[], int displayArraySize)
@@ -157,15 +256,11 @@ void outputArray(uint8_t displayArray[], int displayArraySize)
     // In each for loop, if zigzag, every other time switch order printed
     // choose once, starting loop, based on where first LED is placed:
 
-  if (zigZag == 1) // In order to avoid a check each time through the loop, lines of code are sacrificied to only check it once!
+  if (zigZag == 1) // In order to avoid a check each time through the loop, some code is duplicated here
   {
-    Serial.println("Zigzag = 1");
     // Setup when initial "zig" happens.
     bool zigZagNow = 0;
     if (colDirLeft == 1) zigZagNow = 1;
-    Serial.print("Initial zigZagNow = ");
-    Serial.print(zigZagNow);
-    Serial.print("\n");
     if (rowsDirDown == 1) // Top to bottom LEDs
     {
       //TODO: Implement
